da5f85c1104d49dbce697254790c98bdaf0ba237
rookiecoder34 <ayaydintolga@gmail.com>
2026-02-02 11:44:38 +0300

    First method initiated and completed.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,36 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        System.out.println(isPalindrome(11212));
     }
-}
+
+    public static boolean isPalindrome(int number) {
+
+        if(number<0){
+            number=Math.abs(number);
+        }
+        int num= number;
+        int reversed=0;
+
+        while (num>0){
+
+            int digit=num%10;
+            reversed=reversed*10+digit;
+            num=num/10;
+
+        }
+        reversed=Math.abs(reversed);
+        System.out.println(reversed);
+        System.out.println(number);
+        if(number==reversed){
+            return true;
+        }else {
+            return false;
+        }
+
+    }
+        }
+
+
